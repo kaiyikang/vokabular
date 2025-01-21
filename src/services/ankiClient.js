3b804ac471a5e0d9b0c5f5a2b39668a4d796c1e2
kaiyikang <kaiyikang@gmail.com>
2025-01-21 21:20:14 +0100

    Refactor ankiClient to include additional methods for retrieving deck names, model names, and notes info; update test code for clarity and remove commented-out code

--- src/services/ankiClient.js
+++ src/services/ankiClient.js
@@ -49,31 +49,36 @@ async function invokeAnki(action, params = {}) {
 }
 
 const AnkiClient = {
+    // Deck
     getDeckNames: () => invokeAnki("deckNames"),
+    // Model
     getModelNames: () => invokeAnki("modelNames"),
     getModelTemplatesByName: (modelName) =>
         invokeAnki("modelTemplates", { modelName: modelName }),
+    // Card
     getCardsByDeckName: (deckName) =>
         invokeAnki("findCards", { query: `deck:${deckName}` }),
     getCardsInfo: (cards) => invokeAnki("cardsInfo", { cards: cards }),
+    // Note
     getNotesInfo: (notes) => invokeAnki("notesInfo", { notes: notes }),
+};
+
+module.exports = {
+    AnkiClient,
 };
 
 // 测试代码
 (async () => {
-    const cardIds = await AnkiClient.getCardsByDeckName("Deutsch");
-    const models = await AnkiClient.getModelNames();
-    console.log(models);
-    AnkiClient.getModelTemplatesByName(models[3]).then((templates) => {
-        console.log(templates);
-    });
+    const health = await ankiHealthCheck();
+    // const cardIds = await AnkiClient.getCardsByDeckName("Deutsch");
+    // const models = await AnkiClient.getModelNames();
+    // console.log(models);
+    // AnkiClient.getModelTemplatesByName(models[3]).then((templates) => {
+    //     console.log(templates);
+    // });
     // AnkiClient.getNotesInfo(cardIds).then((cards) => {
     //     cards.map((card) => {
     //         console.log(card.fields);
     //     });
     // });
 })();
-
-module.exports = {
-    AnkiClient,
-};
