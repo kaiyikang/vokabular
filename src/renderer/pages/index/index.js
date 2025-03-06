6ea8998d772b6e45fa352e441f1b6760f2016b28
ValMobYKang <yikai.kang@valtech.com>
2025-03-06 23:32:46 +0100

    Code CleanUp

--- src/renderer/pages/index/index.js
+++ src/renderer/pages/index/index.js
@@ -1,3 +1,4 @@
+const DEBUG = true;
 const inputSentence = document.getElementById("inputSentence");
 const outputExplanation = document.getElementById("outputExplanation");
 const selectedWordDisplay = document.getElementById("selectedWordDisplay");
@@ -5,20 +6,72 @@ const saveToAnkiBtn = document.getElementById("saveToAnkiBtn");
 const clearInputBtn = document.getElementById("clearInputBtn");
 const clipboardToggle = document.getElementById("clipboardToggle");
 
-let clipboardInterval = null;
+// ===== Init =====
 
-function sendToStatusBar(content) {
-    document.getElementById("statusBar").textContent = content;
-}
-
-// When DOM is loaded
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("settingBtn").addEventListener("click", () => {
         window.electronAPI.openSettings();
     });
-    // Anki Health Check
-    updateAnkiButtonState();
+
     setInterval(updateAnkiButtonState, 10000);
+    updateAnkiButtonState();
+});
+
+// ===== Clear Button =====
+
+clearInputBtn.addEventListener("click", () => {
+    inputSentence.value = "";
+    selectedWordDisplay.value = "";
+    outputExplanation.value = "";
+    inputSentence.focus();
+    updateAnkiButtonState();
+    sendToStatusBar("All fields are cleared!");
+    setupClipboardMonitoring();
+});
+
+// ===== Input Sentence =====
+
+inputSentence.addEventListener("input", async (event) => {
+    const text = event.target.value;
+    const singleLineText = text.replace(/\s+/g, " ");
+    inputSentence.value = singleLineText;
+});
+
+inputSentence.addEventListener("dblclick", async (event) => {
+    // Stop the interval since the user will select word
+    stopClipboardMonitoring();
+
+    const trimmedText = window.getSelection().toString().trim();
+    if (trimmedText !== "" && !/^[\s\p{P}]+$/u.test(trimmedText)) {
+        const selectedWord = trimmedText;
+        const inputPhrase = event.target.value.trim();
+        try {
+            lockUI();
+            const response = DEBUG
+                ? "TEST"
+                : await window.services.chat.generateWordExplanation(
+                      inputPhrase,
+                      selectedWord,
+                  );
+            inputSentence.value = inputPhrase
+                .replace(/<[^>]*>/g, "")
+                .replace(new RegExp(`(${selectedWord})`, "gi"), "<b>$1</b>");
+            selectedWordDisplay.value =
+                response
+                    .match(
+                        /<extracted_combination>([\s\S]*?)<\/extracted_combination>/,
+                    )?.[1]
+                    ?.trim() || "Error: Cannot find <extracted_combination>";
+            outputExplanation.value =
+                response
+                    .match(/<explanation>([\s\S]*?)<\/explanation>/)?.[1]
+                    ?.trim() || "Error: Cannot find <explanation>";
+        } catch (error) {
+            sendToStatusBar(`Error: ${error.message}`);
+        } finally {
+            unlockUI();
+        }
+    }
 });
 
 function lockUI() {
@@ -44,73 +97,7 @@ function unlockUI() {
     clearInputBtn.style.backgroundColor = "#ffffff";
 }
 
-clearInputBtn.addEventListener("click", () => {
-    inputSentence.value = "";
-    selectedWordDisplay.value = "";
-    outputExplanation.value = "";
-    inputSentence.focus();
-    updateAnkiButtonState();
-    sendToStatusBar("All fields are cleared!");
-    setupClipboardMonitoring();
-    console.log("clipboardInterval->", clipboardInterval);
-});
-
-inputSentence.addEventListener("input", async (event) => {
-    const text = event.target.value;
-    const singleLineText = text.replace(/\s+/g, " ");
-    inputSentence.value = singleLineText;
-});
-
-inputSentence.addEventListener("dblclick", async (event) => {
-    // Stop the interval since the user will select word
-    if (clipboardInterval) {
-        clearInterval(clipboardInterval);
-        clipboardInterval = null;
-    }
-    const trimmedText = window.getSelection().toString().trim();
-    if (trimmedText !== "" && !/^[\s\p{P}]+$/u.test(trimmedText)) {
-        const selectedWord = trimmedText;
-        const inputPhrase = event.target.value.trim();
-        try {
-            lockUI();
-            const response = await window.services.chat.generateWordExplanation(
-                inputPhrase,
-                selectedWord,
-            );
-            inputSentence.value = inputPhrase
-                .replace(/<[^>]*>/g, "")
-                .replace(new RegExp(`(${selectedWord})`, "gi"), "<b>$1</b>");
-            selectedWordDisplay.value =
-                response
-                    .match(
-                        /<extracted_combination>([\s\S]*?)<\/extracted_combination>/,
-                    )?.[1]
-                    ?.trim() || "Error: Cannot find <extracted_combination>";
-            outputExplanation.value =
-                response
-                    .match(/<explanation>([\s\S]*?)<\/explanation>/)?.[1]
-                    ?.trim() || "Error: Cannot find <explanation>";
-        } catch (error) {
-            sendToStatusBar(`Error: ${error.message}`);
-        } finally {
-            unlockUI();
-        }
-    }
-});
-
-// Anki related functions
-function validateAnkiFieldsBeforeSend(fields) {
-    if (!fields.Sentence?.trim()) {
-        throw new Error("Example Sentence is required");
-    }
-    if (!fields.Word?.trim()) {
-        throw new Error("Selected Word is required");
-    }
-    if (!fields.Definition?.trim()) {
-        throw new Error("Definition is required");
-    }
-    return true;
-}
+// ===== Send To Anki Button =====
 
 saveToAnkiBtn.addEventListener("click", async (event) => {
     try {
@@ -125,13 +112,26 @@ saveToAnkiBtn.addEventListener("click", async (event) => {
         };
         validateAnkiFieldsBeforeSend(ankiFields);
 
-        window.services.anki.addNoteToAnki(ankiFields);
+        DEBUG ? null : window.services.anki.addNoteToAnki(ankiFields);
         sendToStatusBar(`Added Note: ${selectedWordDisplay.value}`);
     } catch (error) {
         sendToStatusBar(`Error: ${error.message}`);
     }
     setupClipboardMonitoring();
 });
+
+function validateAnkiFieldsBeforeSend(fields) {
+    if (!fields.Sentence?.trim()) {
+        throw new Error("Example Sentence is required");
+    }
+    if (!fields.Word?.trim()) {
+        throw new Error("Selected Word is required");
+    }
+    if (!fields.Definition?.trim()) {
+        throw new Error("Definition is required");
+    }
+    return true;
+}
 
 async function updateAnkiButtonState() {
     try {
@@ -148,25 +148,38 @@ async function updateAnkiButtonState() {
     }
 }
 
+// ===== Clipboard Monitoring =====
+
+let clipboardInterval = null;
+
 clipboardToggle.addEventListener("change", async function () {
-    console.log("Toggle of clipboard: " + clipboardToggle.checked);
     setupClipboardMonitoring();
 });
 
 async function setupClipboardMonitoring() {
-    const readFromClipboard = async () => {
+    if (clipboardToggle.checked && clipboardInterval === null) {
+        startClipboardMonitoring();
+    } else if (!clipboardToggle.checked && clipboardInterval) {
+        stopClipboardMonitoring();
+    }
+}
+
+async function startClipboardMonitoring() {
+    clipboardInterval = setInterval(async () => {
         const text = await window.electronAPI.getClipboardText();
         if (text && inputSentence.text !== text) {
             inputSentence.value = text.replace(/\s+/g, " ");
         }
-    };
-    if (clipboardToggle.checked) {
-        clipboardInterval = setInterval(readFromClipboard, 1000);
-        // readFromClipboard();
-    } else {
-        if (clipboardInterval) {
-            clearInterval(clipboardInterval);
-            clipboardInterval = null;
-        }
-    }
+    }, 1000);
+}
+
+async function stopClipboardMonitoring() {
+    clearInterval(clipboardInterval);
+    clipboardInterval = null;
+}
+
+// ===== Status Bar =====
+
+function sendToStatusBar(content) {
+    document.getElementById("statusBar").textContent = content;
 }
